23cb7cc47745109d841a569e138e2dd5b3db0cbd
vineemon <nadella.vineet@gatech.edu>
2019-09-02 14:12:02 -0400

    Add files via upload

--- sram_trace_generator.py
+++ sram_trace_generator.py
@@ -1,11 +1,10 @@
 import numpy as np
 
-####input is s1, s2, T
-#####output is the ifmap trace, filter trace, and ofmap trace
-#hyperparameters: systolic array size (cols, rows)
-#hyper_parameter: s1 (in this case, pixels in filter)
-#hyper_parameter: s2 (in this case, # of filters)
-#hyper_parameter: T (in this case ofmap pixels (=(ifmap_rows-filter_rows+1)/1))
+# output is the ifmap trace, filter trace, and ofmap
+# hyper parameters: systolic array size (cols, rows)
+# hyper_parameter: s1 (pixels in filter)
+# hyper_parameter: s2 (# of filters)
+# hyper_parameter: T (ofmap pixels in one channel (=(ifmap_rows-filter_rows+stride)/stride))
 class simulation:
     def __init__(self, s2, T):
         self.left_column = np.zeros((1, T))
@@ -13,77 +12,110 @@ class simulation:
 
 
 class sram_trace_generator:
-    def __init__(self):
-        self.filt_h = 3
-        self.filt_w = 3
-        self.ifmap_h = 5
-        self.ifmap_w = 5
-        self.ofmap_h = 3
-        self.ofmap_w = 3
-
-    sysarray_rows = 4
-    sysarray_cols = 4
-    top_row = np.zeros((1, sysarray_cols))
-    left_col = np.zeros((1, sysarray_rows))
-    cycles = 0
+    def __init__(self, stride=1, filt_h=2, filt_w=2,
+                 ifmap_h=3, ifmap_w=3, num_filters=4, num_channels=1):
+        self.stride = stride  # input
+        self.filt_h = filt_h  # input
+        self.filt_w = filt_w  # input
+        self.num_channels = num_channels  # input
+        self.ifmap_h = ifmap_h  # input
+        self.ifmap_w = ifmap_w  # input
+        self.num_filters = num_filters  # input
+        self.ofmap_h = int((self.ifmap_h - self.filt_h + self.stride)/self.stride)
+        self.ofmap_w = int((self.ifmap_w - self.filt_w + self.stride)/self.stride)
+        self.s1 = self.filt_h * self.filt_w * self.num_channels
+        self.s2 = self.num_filters
+        self.T = self.ofmap_h * self.ofmap_w
 
     def simulate_sram_trace(self, input, filter):
+        output = np.ones((self.T + self.s2 - 1, self.s2))*(-1)
+        outputBase = 0
+        outputPixel = outputBase
         simArray = []
-        for cycle in range(min(len(input[0]), len(filter[0]))):
-            sim = simulation(s2=len(filter), T=len(input))
-            sim.left_column = np.transpose(input[:,cycle])
-            sim.top_row = np.transpose(filter[:,cycle])
-            simArray.append(sim)
-        return simArray
+        filtersArray = []
+        start = False
+        increasing = True
+        for cycle in range(self.T + self.s2 + self.s1):
+            if cycle < min(len(input[0]), len(filter[0])):
+                sim = simulation(s2=self.s2, T=self.T)
+                sim.left_column = np.transpose(input[:, cycle])
+                sim.top_row = np.transpose(filter[:, cycle])
+                simArray.append(sim)
+            outputCycle = cycle - self.s1
+            if outputCycle == 0:
+                start = True
+            elif outputCycle == self.T:
+                increasing = False
+            if start:
+                if increasing:
+                    filtersArray.append(len(filtersArray))
+                elif len(filtersArray) > 0:
+                    filtersArray.pop(0)
+                    outputPixel = outputPixel + self.T - 1
+                for i in range(len(filtersArray)):
+                    output[outputCycle][filtersArray[i]] = outputPixel + ((self.T - 1) * i)
+                outputPixel = outputPixel + 1
+        return simArray, output
 
 
-    #def noSkewMat(inpMatrix):
+    def noSkewMat(self, inpMatrix):
+        returnMatrix = np.ones(((len(inpMatrix) - len(inpMatrix[0]) + 1), len(inpMatrix[0])))
+        for i in range(len(returnMatrix)):
+            for j in range(len(returnMatrix[0])):
+                returnMatrix[i][j] = inpMatrix[i + j][j]
+        return returnMatrix
 
 
     def skewMat(self, inpMatrix):
-        returnMatrix = np.ones((len(inpMatrix),(len(inpMatrix[0])+len(inpMatrix)-1)))*-1
+        returnMatrix = np.ones((len(inpMatrix), (len(inpMatrix[0])+len(inpMatrix)-1)))*-1
         for i in range(len(inpMatrix)):
             for j in range(len(inpMatrix[0])):
                 returnMatrix[i][i + j] = inpMatrix[i][j]
         return returnMatrix
 
 
-    def read_trace_arrays_os(self, s1, s2, T, stride):
-        filter = np.zeros((s2, s1))
-        ifmap = np.zeros((T, s1))
+    def read_trace_arrays_os(self):
+        filter = np.zeros((self.s2, self.s1))
         filter_base = 0
-        temp_filt = filter_base
-        ##populate the filter input matrix
-        for i in range(s2):
-            for j in range(s1):
-                filter[i][j] = temp_filt
-                temp_filt = temp_filt + 1
+        # populate the filter input matrix
+        for i in range(self.s2):
+            for j in range(self.s1):
+                filter[i][j] = filter_base + i*self.s1 + j
 
-        ##populate the ifmap input matrix
+        # populate the ifmap input matrix
+        ifmap = np.zeros((self.T, self.s1))
         input_base = 0
         temp_ifmap = input_base
         first_address = temp_ifmap
-        start_row = input_base
-        for i in range(T):
-            for j in range(s1):
+        for i in range(self.T):
+            for j in range(self.s1):
                 ifmap[i][j] = temp_ifmap
                 temp_ifmap = temp_ifmap + 1
                 if (j + 1) % self.ofmap_w == 0:
                     temp_ifmap = int(temp_ifmap/self.ifmap_w) + self.ifmap_w
             if (i + 1) % self.ofmap_h == 0:
-                first_address = int(first_address/self.ifmap_w) + self.ifmap_w*stride
+                first_address = int(first_address/self.ifmap_w) + self.ifmap_w*self.stride
             else:
-                first_address = first_address + stride
+                first_address = first_address + self.stride
             temp_ifmap = first_address
         return ifmap, filter
 
 if __name__ == "__main__":
     s = sram_trace_generator()
-    (ifmap, filter) = s.read_trace_arrays_os(9, 4, 9, stride = 1)
+    (ifmap, filter) = s.read_trace_arrays_os()
     input = s.skewMat(ifmap)
     filter = s.skewMat(filter)
-    simArray = s.simulate_sram_trace(input, filter)
-    print("herel")
+    (simArray, output) = s.simulate_sram_trace(input, filter)
+    unskewedOutput = s.noSkewMat(output)
+    for i in range(len(simArray)):
+        print("Inputs at Cycle " + str(i) + ":")
+        print(simArray[i].left_column)
+        print("Filters at Cycle " + str(i) + ":")
+        print(simArray[i].top_row)
+    print("Outputs Skewed: ")
+    print(output)
+    print("Outputs Unskewed: ")
+    print(unskewedOutput)
 
 
 
